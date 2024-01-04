8f4be96e7296be74a56ef7d1cc615d7d3cbe6332
Maialen Muñoa <mmunoa@yahoo.com>
2024-01-04 23:58:23 +0100

    add some favourites styles

--- src/js/main.js
+++ src/js/main.js
@@ -14,26 +14,39 @@ const charactersFavouritesUl = document.querySelector('.js__charactersFavourites
 //DATA
 
 let charactersData = [];
-let favouritesData = [];
+const favouritesData = [];
 
 
 //FUNCTIONS
 
 function renderOne(charactersData) {
-    charactersResultUL.innerHTML += `
+    const favouriteCharacterIndex = favouritesData.findIndex(oneCharacter => oneCharacter.id === charactersData.id );
+
+    if(favouriteCharacterIndex === -1) {
+        charactersResultUL.innerHTML += `
+        <li class="characters__item js__allCharactersLi" data_id=${charactersData.id}>
+            <img class="characters__image" src="${charactersData.imageUrl}" alt="Foto de ${charactersData.name}"></img>
+            <h3 class="characters__name">${charactersData.name}</h3>
+        </li>
+        `;
+    } else {
+        charactersResultUL.innerHTML += `
         <li class="characters__item js__allCharactersLi selected" data_id=${charactersData.id}>
             <img src="${charactersData.imageUrl}" alt="Foto de ${charactersData.name}"></img>
             <h3>${charactersData.name}</h3>
         </li>
         `;
+    }
 }
 
 function renderAll() {
+    charactersResultUL.innerHTML = '';
     for (const eachCharacter of charactersData) {
         renderOne(eachCharacter);
     }
 
     const allCharactersLi = document.querySelectorAll('.js__allCharactersLi');
+
 
     for(const eachLi of allCharactersLi) {
         eachLi.addEventListener('click', handleCharacterClick);
@@ -53,7 +66,7 @@ function renderFavourites() {
     charactersFavouritesUl.innerHTML = '';
 
     for (const eachFavourite of favouritesData) {
-        renderOne(eachFavourite);
+        renderOneFavourite(eachFavourite);    
     }
 }
 
@@ -62,27 +75,25 @@ function renderFavourites() {
 function handleCharacterClick(event) {
 
     const clickedLi = event.currentTarget;
-    clickedLi.classList.add('hidden');
-    charactersFavouritesUl.classList.remove('hidden');
-
     const clickedCharacterId = clickedLi.dataset.id;
-
     const selectedCharacterData = charactersData.find(oneCharacter => oneCharacter.id === clickedCharacterId);
-
-    const favouriteCharacterIndex = favouritesData.findIndex(oneCharacter => oneCharacter.id === clickedCharacterId);
-
+    const favouriteCharacterIndex = favouritesData.findIndex(oneCharacter => oneCharacter.id === clickedCharacterId );  
+    
     if(favouriteCharacterIndex === -1) {
         favouritesData.push(selectedCharacterData);
     } else {
         favouritesData.splice(favouriteCharacterIndex, 1);
     }
+   
+    renderFavourites(); 
 
-    renderFavourites();
-    clickedLi.classList.add('selected');
+    clickedLi.classList.remove('hidden');
+    charactersFavouritesUl.classList.remove('hidden');
+    clickedLi.classList.toggle('selected');
 }
 
 
-//EVENTS       
+//EVENTS   
 // searchForm.addEventListener('submit', function (event) {
 //     event.preventDefault();
 //     console.log('search form submitted');
@@ -96,5 +107,5 @@ fetch('//api.disneyapi.dev/character?pageSize=50')
 
        charactersData = data.data;
 
-       renderAll();
+       renderAll(charactersData);
     });
