e2fee8437d25d0fd2b87ffe328967166ea94dbb7
Maialen Muñoa <mmunoa@yahoo.com>
2024-01-04 12:25:05 +0100

    add improvments in design

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,43 @@
 'use strict';
+// import './variables.js'
+// import './list.js'
+// import './favourites.js'
+// import './search.js'
+// import './reset.js'
 
-import './variables.js'
-import './list.js'
-import './favourites.js'
-import './search.js'
-import './reset.js'
+//QUERY SELECTORS
+const charactersUL = document.querySelector('.characters__list');
 
-console.log('>> Ready :)');
+//DATA
+
+
+//FUNCTIONS
+<li class="characters__item">
+<img src="https://placehold.co/190x230" alt="Foto de Ariel"></img>
+<p>Ariel</p>
+</li>
+<li class="characters__item">
+<img src="https://placehold.co/190x230" alt="Foto de Olu Mel"></img>
+<p>Ariel</p>
+</li>
+<li class="characters__item">
+<img src="https://placehold.co/190x230" alt="Foto de Olu Mel"></img>
+<p>Ariel</p>
+</li>
+<li class="characters__item">
+<img src="https://placehold.co/190x230" alt="Foto de Olu Mel"></img>
+<p>Ariel</p>
+</li>
+
+//FUNCTIONS/EVENTS (HANDLER)
+
+
+//EVENTS       
+searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log('search form submitted');
+}); 
+
+//CODE TO RUN ON PAGE LOAD
+
 
